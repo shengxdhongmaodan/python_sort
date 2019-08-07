128e371490a68422286bf1095d21f4687bfbf478
shengxdhongmaodan <740693424@qq.com>
2019-08-07 10:55:31 +0800

    Create quick_sort.py
    add comment

---
+++ quick_sort.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@author  : shengxd
+@Contact : 740693424@qq.com
+@File    :   quick_sort.py
+@Time    :   2019/8/7 10:38
+@Desc    :
+
+"""
+
+
+def quick_sort(my_list):
+    return my_list
+
+
+if __name__ == "__main__":
+    my_list = [9, 7, 2, 3, 8, 1, 5]
+    quick_sort(my_list)
+
