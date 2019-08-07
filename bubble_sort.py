cd014dfbf86eeda15b2a662f17499e203a86ab67
shengxdhongmaodan <740693424@qq.com>
2019-08-07 10:16:52 +0800

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 
+# 冒泡排序
 
-def bubble_sort(mylist):
-    len_mylist = len(mylist)
-    num_of_compare_round = 0
-    sort_complete_num = 0
-    for i in range(len_mylist):
+
+def bubble_sort(my_list):
+    """
+
+    :param my_list:
+    :return:
+    """
+    len_my_list = len(my_list)
+    num_of_compare_round = 0  # 统计比较的总次数
+    sort_complete_num = 0     # 优化冒泡排序的判断条件：当比较完一轮，发现已排好序，不再进行下一轮比较
+    for i in range(len_my_list):
         num_of_compare_round2 = 0
-        for j in range(len_mylist-i-1):
+        for j in range(len_my_list-i-1):
             num_of_compare_round2 += 1
             if mylist[j] > mylist[j+1]:
                 mylist[j], mylist[j+1] = mylist[j+1], mylist[j]
             else:
                 sort_complete_num += 1
         num_of_compare_round += num_of_compare_round2
-        if sort_complete_num == len_mylist-i-1:
+        if sort_complete_num == len_my_list-i-1:
             print("sort has completed")
             break
     print("num_of_compare_round {}".format(num_of_compare_round))
